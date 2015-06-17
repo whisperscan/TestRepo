688bd59b0e37fc56bc2e5bf02616ece742eae960
caramel <skynets@yeah.net>
2015-06-17 10:52:09 +0800

    modified head.h and create main.c

--- head.h
+++ head.h
@@ -1,10 +1,1 @@
 #include <stdio.h>
-
-int main(int argc, char const *argv[])
-{
-	for (int i = 0; i < 10; ++i)
-	{
-		
-	}
-	return 0;
-}
---
+++ main.c
@@ -0,0 +1,8 @@
+int main(int argc, char const *argv[])
+{
+	for (int i = 0; i < 10; ++i)
+	{
+		
+	}
+	return 0;
+}
